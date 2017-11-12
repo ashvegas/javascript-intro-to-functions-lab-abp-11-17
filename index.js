684112b22e1c23395125b39fc75fac2dfc3ab645
AC <msashvegas@gmail.com>
2017-11-12 22:10:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,19 +15,7 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase() === string){
-    return "I can\'t hear you!";
-  }
-}
-
-function sayHiToGrandma(string){
-  if (string.toUpperCase() === string){
-    return 'YES INDEED!';
-  }
-}
-
-function sayHiToGrandma(string){
-  if (string === 'I love you, Grandma.'){
-    return "I love you, too.";
+  if (string === string.toLowerCase()){
+    return "I can't hear you!";
   }
 }
